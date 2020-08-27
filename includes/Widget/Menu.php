1bf012e618a0fd91fb3d0ae38fdaaee5da6cdeb6
Rafael Ramos <rramo012@gmail.com>
2020-08-27 15:27:14 -0400

    Menu component

---
+++ includes/Widget/Menu.php
@@ -0,0 +1,106 @@
+<?php
+/**
+* File: Single.php
+*
+* Create a post component.
+*
+* @since      1.0.0
+* @package    Boldgrid_Components
+* @subpackage Boldgrid_Components_Shortcode
+* @author     BoldGrid <support@boldgrid.com>
+* @link       https://boldgrid.com
+*/
+
+namespace Boldgrid\PPB\Widget;
+
+/**
+* Class: Single
+*
+* Create a post component.
+*
+* @since 1.0.0
+*/
+class Menu extends \WP_Widget {
+
+	/**
+	 * Default widget wrappers.
+	 *
+	 * @since 1.0.0
+	 * @var array
+	 */
+	public static $widgetArgs = array(
+		'before_title' => '',
+		'after_title' => '',
+		'before_widget' => '<div class="widget">',
+		'after_widget' => '</div>',
+	);
+
+	/**
+	 * Default values.
+	 *
+	 * @since 1.0.0
+	 * @var array Default values.
+	 */
+	public $defaults = [
+	];
+
+		/**
+	 * Setup the widget configurations.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		parent::__construct(
+			'boldgrid_component_menu',
+			__( 'BoldGrid Menu', 'boldgrid-editor' ),
+			array(
+				'classname' => 'bgc-menu',
+				'description' => __( 'A customizable menu.', 'boldgrid-editor' )
+			)
+		);
+	}
+
+	/**
+	 * Update a widget with a new configuration.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param  array $new_instance New instance configuration.
+	 * @param  array $old_instance Old instance configuration.
+	 * @return array               Updated instance config.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = $new_instance;
+
+		return $instance;
+	}
+
+	/**
+	 * Render a widget.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param  array $args     General widget configurations.
+	 * @param  array $instance Widget instance arguments.
+	 */
+	public function widget( $args, $instance )  {
+		echo "based on your configiguration your menu looks like this";
+	}
+
+
+	/**
+	 * Print our a form that allowing the widget configs to be updated.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param  array $instance Widget instance configs.
+	 */
+	public function form( $instance ) {
+		?>
+		<p>
+			<h4><?php _e( 'Create a custom menu control set here:', 'boldgrid-editor' ) ?></h4>
+		</p>
+	<?php
+	}
+
+}
