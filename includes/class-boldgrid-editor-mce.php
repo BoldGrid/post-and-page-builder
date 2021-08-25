5570d316f86e1a67250a62a2f5a300b132f364eb
jamesros <jamesros@boldgrid.com>
2021-08-25 13:21:27 -0400

    changed Add Block button to use wp default (#207)

--- includes/class-boldgrid-editor-mce.php
+++ includes/class-boldgrid-editor-mce.php
@@ -51,7 +51,7 @@ class Boldgrid_Editor_MCE {
 	 * @since 1.1.
 	 */
 	public function load_editor_hooks() {
-		echo '<button type="button" id="insert-gridblocks-button" class="button gridblock-icon boldgrid-color hidden">' .
+		echo '<button type="button" id="insert-gridblocks-button" class="button-primary gridblock-icon hidden">' .
 			'<span class="wp-media-buttons-icon"></span> Add Block</button>';
 	}
 
