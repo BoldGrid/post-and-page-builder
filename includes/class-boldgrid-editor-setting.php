43f48fa21b9b7723cbea792dd45eed0e366a8956
James Rosado <jamesros@boldgrid.com>
2021-09-14 10:32:58 -0400

    add intval to $_GET['post_id']

--- includes/class-boldgrid-editor-setting.php
+++ includes/class-boldgrid-editor-setting.php
@@ -261,7 +261,7 @@ class Boldgrid_Editor_Setting {
 			$default_editor = get_post_meta( $post->ID, '_bgppb_default_editor', true ) ?: $default_editor;
 		} elseif ( ! $default_editor_override && ! $post && isset( $_GET['post_id'] ) ) {
 			// When loading iFrames in the add media tabs, the $post variable is not set, so we have to obtain the ID from the $_GET array.
-			$default_editor = get_post_meta( $_GET['post_id'], '_bgppb_default_editor', true ) ?: $default_editor;
+			$default_editor = get_post_meta( intval( $_GET['post_id'] ), '_bgppb_default_editor', true ) ?: $default_editor;
 		}
 
 		return $default_editor;
