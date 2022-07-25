36b50ac8cc22ddb8bb2d1baf08f7b097f39e851e
James Rosado <jamesros@boldgrid.com>
2022-07-25 16:43:14 -0400

    add table panel

---
+++ assets/js/builder/controls/element/tables.js
@@ -0,0 +1,359 @@
+window.BOLDGRID = window.BOLDGRID || {};
+BOLDGRID.EDITOR = BOLDGRID.EDITOR || {};
+BOLDGRID.EDITOR.CONTROLS = BOLDGRID.EDITOR.CONTROLS || {};
+
+( function( $ ) {
+	'use strict';
+
+	var self,
+		BG = BOLDGRID.EDITOR;
+
+	BOLDGRID.EDITOR.CONTROLS.Tables = {
+		name: 'tables',
+
+		tooltip: 'Table Designer',
+
+		uploadFrame: null,
+
+		priority: 10,
+
+		/**
+		 * Currently controlled element.
+		 *
+		 * @type {$} Jquery Element.
+		 */
+		$target: null,
+
+		/**
+		 * Tracking of clicked elements.
+		 * @type {Object}
+		 */
+		layerEvent: { latestTime: 0, targets: [] },
+
+		elementType: '',
+
+		iconClasses: 'dashicons dashicons-editor-table',
+
+		selectors: [ 'table', 'div[class*="col"]' ],
+
+		defaults: {
+			cols: 3,
+			rows: 4
+		},
+
+		/**
+		 * Initialize control.
+		 *
+		 * @since 1.17.0
+		 */
+		init: function() {
+			BOLDGRID.EDITOR.Controls.registerControl( this );
+		},
+
+		panel: {
+			title: 'Table Designer',
+			height: '600px',
+			width: '450px',
+			includeFooter: true,
+			customizeLeaveCallback: true,
+			customizeCallback: true,
+			customizeSupport: [
+				'margin',
+				'box-shadow',
+				'table-borders',
+				'animation',
+				'background-color',
+				'customClasses'
+			]
+		},
+
+		/**
+		 * Get the color from defaults
+		 *
+		 * @since 1.17.0
+		 *
+		 * @param {string} color color value
+		 * @returns color value
+		 */
+		getColorFromPalette: function( color ) {
+			var paletteColors = BoldgridEditor.colors,
+				isPaletteColor = false,
+				isNeutralColor;
+
+			isNeutralColor = BOLDGRID.COLOR_PALETTE.Modify.compareColors( color, paletteColors.neutral );
+
+			if ( false !== isNeutralColor ) {
+				return 'var( --color-neutral )';
+			}
+
+			paletteColors.defaults.forEach( function( paletteColor, index ) {
+				var isMatch = BOLDGRID.COLOR_PALETTE.Modify.compareColors( color, paletteColor );
+				if ( isMatch ) {
+					isPaletteColor = `var(--color-${index + 1} )`;
+				}
+			} );
+
+			return false !== isPaletteColor ? isPaletteColor : color;
+		},
+
+		/**
+		 * Get the current target.
+		 *
+		 * @since 1.8.0
+		 * @return {jQuery} Element.
+		 */
+		getTarget: function() {
+			return self.$target;
+		},
+
+		/**
+		 * When the user clicks on a menu item, update the available options.
+		 *
+		 * @since 1.8.0
+		 */
+		onMenuClick: function() {
+			var initialTarget = BOLDGRID.EDITOR.Menu.getTarget( self );
+			self.createTable( initialTarget );
+			self.openPanel();
+		},
+
+		/**
+		 * Open the editor panel for a given selector and store element as target.
+		 *
+		 * @since 1.8.0
+		 *
+		 * @param  {string} selector Selector.
+		 */
+		open: function( selector ) {
+			for ( let target of self.layerEvent.targets ) {
+				console.log( {
+					target: target
+				} );
+				let $target = $( target );
+				if ( $target.is( selector ) ) {
+					self.openPanel( $target );
+				}
+			}
+		},
+
+		/**
+		 * When the user clicks on an element within the mce editor record the element clicked.
+		 *
+		 * @since 1.8.0
+		 *
+		 * @param  {object} event DOM Event
+		 */
+		elementClick( event ) {
+			if ( self.layerEvent.latestTime !== event.timeStamp ) {
+				self.layerEvent.latestTime = event.timeStamp;
+				self.layerEvent.targets = [];
+			}
+
+			self.layerEvent.targets.push( event.currentTarget );
+		},
+
+		/**
+		 * Setup Init.
+		 *
+		 * @since 1.2.7
+		 */
+		setup: function() {
+			self.$menuItem = BG.Menu.$element.find( '[data-action="menu-tables"]' );
+
+			self._setupMenuReactivate();
+		},
+
+		/**
+		 * When a menu item is reopened because a user clicked on another similar element
+		 * Update the available options.
+		 *
+		 * @since 1.8.0
+		 */
+		_setupMenuReactivate: function() {
+			self.$menuItem.on( 'reactivate', self.updateMenuOptions );
+		},
+
+		/**
+		 * Add style to header.
+		 *
+		 * @since 1.17.0
+		 *
+		 * @param {string} styleId style id
+		 * @param {string} css css
+		 */
+		_addHeadingStyle: function( styleId, css ) {
+			var $target = self.getTarget(),
+				$body = $target.parents( 'body' ),
+				$head = $body.parent().find( 'head' );
+
+			if ( $head.find( '#' + styleId ).length ) {
+				$head.find( '#' + styleId ).remove();
+			}
+
+			$head.append( '<style id="' + styleId + '">' + css + '</style>' );
+		},
+
+		/**
+		 * Determine the element type supported by this control.
+		 *
+		 * @since 1.8.0
+		 *
+		 * @param  {jQuery} $element Jquery Element.
+		 * @return {string}          Element.
+		 */
+		checkElementType: function( $element ) {
+			let type = '';
+			if ( $element.is( 'TABLE' ) ) {
+				type = 'table';
+			} else if ( $element.hasClass( 'row' ) ) {
+				type = 'row';
+			} else {
+				type = 'column';
+			}
+
+			return type;
+		},
+
+		/**
+		 * Open Panel.
+		 *
+		 * @since 1.2.7
+		 *
+		 * @param $target Current Target.
+		 */
+		openPanel: function( $target ) {
+			var panel = BG.Panel,
+				template = wp.template( 'boldgrid-editor-tables' );
+
+			// Remove all content from the panel.
+			panel.clear();
+			panel.$element.find( '.panel-body' ).html( template( self.getTemplateVariables() ) );
+
+			self._setupChangeOptions();
+
+			// Open Panel.
+			panel.open( self );
+		},
+
+		/**
+		 * Get Variables for use in template.
+		 *
+		 * @since 1.17.0
+		 *
+		 * @returns {array} array of template variables.
+		 */
+		getTemplateVariables: function() {
+			var variables = {
+				states: {}
+			};
+
+			return variables;
+		},
+
+		createTable: function( initialTarget ) {
+			var selection = BOLDGRID.EDITOR.mce.selection,
+				$selection = $( selection.getNode() ),
+				$parentDiv = $selection.parents( 'div[class*=col]' ),
+				nodeType = $selection.prop( 'nodeName' ),
+				talbeNodeTypes = [ 'TABLE', 'THEAD', 'TBODY', 'TR', 'TD', 'TH' ],
+				content;
+
+			if (
+				-1 === talbeNodeTypes.indexOf( nodeType ) &&
+				! $selection.is( '[class*=col]' ) &&
+				$parentDiv.length
+			) {
+				console.log( 'Not a table element or a column' );
+				selection.select( $selection.parents( 'div[class*=col]' )[0] );
+				$selection = $( selection.getNode() );
+				nodeType = $selection.prop( 'nodeName' );
+			}
+
+			if ( -1 === talbeNodeTypes.indexOf( nodeType ) ) {
+				content = selection.getContent();
+				content = self.getDefaultTableMarkup() + content;
+				selection.setContent( content );
+				self.$target = $( selection.getNode() ).find( 'table' );
+			} else if ( 'TABLE' === nodeType ) {
+				self.$target = $selection;
+			} else {
+				self.$target = $selection.parents( 'table' );
+			}
+
+			console.log( { selection, $selection, nodeType } );
+		},
+
+		_setupChangeOptions: function() {
+			var panel = BG.Panel,
+				$target = self.getTarget(),
+				$optionInputs = panel.$element.find( '.section-general-options input' );
+
+			console.log( { target: $target } );
+
+			$optionInputs.on( 'change', function() {
+				var $this = $( this ),
+					isChecked = $this.prop( 'checked' ) ? true : false;
+
+				console.log( {
+					inputName: $this.attr( 'name' ),
+					inputValue: $this.val(),
+					isChecked: isChecked
+				} );
+
+				if ( 'radio' === $this.attr( 'type' ) ) {
+					$target.removeClass( $this.attr( 'data-classes' ) );
+				}
+
+				if ( isChecked ) {
+					$target.addClass( $this.val() );
+				} else {
+					$target.removeClass( $this.val() );
+				}
+			} );
+		},
+
+		/**
+		 * Default Table Markup.
+		 *
+		 * @since SINCEVERSION
+		 *
+		 * @returns {string} default table markup.
+		 */
+		getDefaultTableMarkup: function() {
+			return `<table class="table">
+                    <thead>
+                        <tr>
+                            <th data-column="1">Header 1</th>
+                            <th data-column="2">Header 2</th>
+                            <th data-column="3">Header 3</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr data-row="1">
+                            <td data-column="1">Cell 1</td>
+                            <td data-column="2">Cell 2</td>
+                            <td data-column="3">Cell 3</td>
+                        </tr>
+                        <tr data-row="2">
+                            <td data-column="1">Cell 1</td>
+                            <td data-column="2">Cell 2</td>
+                            <td data-column="3">Cell 3</td>
+                        </tr>
+                        <tr data-row="3">
+                            <td data-column="1">Cell 1</td>
+                            <td data-column="2">Cell 2</td>
+                            <td data-column="3">Cell 3</td>
+                        </tr>
+                        <tr data-row="4">
+                            <td data-column="1">Cell 1</td>
+                            <td data-column="2">Cell 2</td>
+                            <td data-column="3">Cell 3</td>
+                        </tr>
+                    </tbody>
+                </table>`;
+		}
+	};
+
+	BOLDGRID.EDITOR.CONTROLS.Tables.init();
+	self = BOLDGRID.EDITOR.CONTROLS.Tables;
+} )( jQuery );
---
+++ includes/template/tables.php
@@ -0,0 +1,56 @@
+<script type="text/html" id="tmpl-boldgrid-editor-tables">
+    <div class="boldgrid-editor-tables presets supports-customization">
+        <div class="section section-columns-and-rows">
+            <p class="number-of-columns">
+                <label for="tables-number-of-columns">Number of Rows</label>
+                <input type="number" name="tables-number-of-columns" id="tables-number-of-columns" value="3" min="1" max="100" step="1">
+            </p>
+            <p class="tables-number-of-rows">
+                <label for="tables-number-of-rows">Number of Rows</label>
+                <input type="number" name="tables-number-of-rows" id="tables-number-of-rows" value="4" min="1" max="100" step="1">
+            </p>
+        </div>
+        <div class="section section-general-options">
+            <p class="hide-header">
+                <label for="tables-hide-header">Hide Header</label>
+                <input type="checkbox" name="tables-hide-header" id="tables-hide-header" value="hide-header">
+            </p>
+            <p class="striped-rows">
+                <label for="tables-striped-rows">Striped Rows</label>
+                <input type="checkbox" name="tables-striped-rows" id="tables-striped-rows" value="table-striped">
+            </p>
+            <p class="hover-rows">
+                <label for="tables-hover-rows">Hover Rows</label>
+                <input type="checkbox" name="tables-hover-rows" id="tables-hover-rows" value="table-hover">
+            </p>
+            <p class="table-text-align">
+                <label>
+                    <input type="radio" data-classes="table-text-align-center table-text-align-right " name="tables-text-align" value="">
+                    Left
+                </label>
+                <label>
+                <input type="radio" data-classes="table-text-align-center table-text-align-right " name="tables-text-align" value="table-text-align-center">
+                    Center
+                </label>
+                <label>
+                <input type="radio" data-classes="table-text-align-center table-text-align-right " name="tables-text-align" value="table-text-align-right">
+                    Right
+                </label>
+            </p>
+            <p class="table-borders">
+                <label>
+                    <input type="radio" data-classes="table-borderless table-bordered" name="tables-borders" value="">
+                    Row Borders
+                </label>
+                <label>
+                    <input type="radio" data-classes="table-borderless table-bordered" name="tables-borders" value="table-borderless">
+                    No Borders
+                </label>
+                <label>
+                    <input type="radio" data-classes="table-borderless table-bordered" name="tables-borders" value="table-bordered">
+                    All Borders
+                </label>
+            </p>
+        </div>
+    </div>
+</script>
