6d059acd75c5ad6b9be70b9a3402ea89b931476b
jamesros <jamesros@boldgrid.com>
2023-05-15 16:16:27 -0400

    Update deps (#529)
    * begin updating deps
    
    * remove pre-commit
    
    * update builds
    
    * add cleanwebpack
    
    * fix gulp
    
    * update controls
    
    * add removePlugin
    
    * add cleanup to webpack
    
    * update bg control dep
    
    * update form.js
    
    * fix overflow
    
    * remove unused dev deps
    
    * remove unused deps
    
    * update travis.yml
    
    * update deps
    
    * disable eslint for now

--- assets/js/builder/notice/intro/notice.js
+++ assets/js/builder/notice/intro/notice.js
@@ -1,4 +1,4 @@
-var $ = window.jQuery,
+const $ = window.jQuery,
 	BG = BOLDGRID.EDITOR;
 
 import './style.scss';
@@ -18,17 +18,17 @@ export class Notice extends Base {
 			height: '285px',
 			width: '650px',
 			disabledClose: true,
-			autoCenter: true
+			autoCenter: true,
 		};
 
 		this.stepConfig = {
-			boldgridTheme: [ 'welcome', 'default-editor', 'done' ],
-			standard: [ 'welcome', 'default-editor', 'choose-template', 'done' ]
+			boldgridTheme: ['welcome', 'default-editor', 'done'],
+			standard: ['welcome', 'default-editor', 'choose-template', 'done'],
 		};
 
-		this.steps = BoldgridEditor.is_boldgrid_theme ?
-			this.stepConfig.boldgridTheme :
-			this.stepConfig.standard;
+		this.steps = BoldgridEditor.is_boldgrid_theme
+			? this.stepConfig.boldgridTheme
+			: this.stepConfig.standard;
 	}
 
 	/**
@@ -39,20 +39,20 @@ export class Notice extends Base {
 	init() {
 		this.defaultEditor = new DefaultEditor();
 		this.selection = new ColorPaletteSelection();
-		this.$body = $( 'body' );
+		this.$body = $('body');
 		this.settings = this.getDefaults();
 
-		this.templateMarkup = _.template( templateHtml )( {
+		this.templateMarkup = _.template(templateHtml)({
 			nonce: BoldgridEditor.setupNonce,
-			stepper: this.getStepper()
-		} );
-		this.$panelHtml = $( this.templateMarkup );
-		this.$panelHtml.find( 'default-editor-form' ).replaceWith( this.defaultEditor.getForm() );
-		this.$templateInputs = this.$panelHtml.find( 'input[name="bgppb-template"]' );
+			stepper: this.getStepper(),
+		});
+		this.$panelHtml = $(this.templateMarkup);
+		this.$panelHtml.find('default-editor-form').replaceWith(this.defaultEditor.getForm());
+		this.$templateInputs = this.$panelHtml.find('input[name="bgppb-template"]');
 
 		this.openPanel();
 		this._setupNav();
-		this._addPanelSettings( 'welcome' );
+		this._addPanelSettings('welcome');
 		this.bindDismissButton();
 		this._setupStepActions();
 	}
@@ -62,31 +62,31 @@ export class Notice extends Base {
 	 *
 	 * @since 1.9.0
 	 *
-	 * @return {object} Template methods.
+	 * @return {Object} Template methods.
 	 */
 	getStepper() {
-		let self = this;
+		const self = this;
 
 		return {
-			getLabel( step ) {
-				let size = self.steps.length,
-					current = self.steps.findIndex( el => step === el ) + 1;
+			getLabel(step) {
+				const size = self.steps.length,
+					current = self.steps.findIndex((el) => step === el) + 1;
 
 				return `Step: ${current}/${size}`;
 			},
-			getNext( step ) {
-				let current = self.steps.findIndex( el => step === el ) + 1;
+			getNext(step) {
+				const current = self.steps.findIndex((el) => step === el) + 1;
 
 				return self.steps[current];
-			}
+			},
 		};
 	}
 
 	getDefaults() {
 		return {
 			template: {
-				choice: 'fullwidth'
-			}
+				choice: 'fullwidth',
+			},
 		};
 	}
 
@@ -97,22 +97,20 @@ export class Notice extends Base {
 	 */
 	openPanel() {
 		BG.Panel.currentControl = this;
-		BG.Panel.setDimensions( this.panel.width, this.panel.height );
-		BG.Panel.setTitle( this.panel.title );
-		BG.Panel.setContent( this.$panelHtml );
+		BG.Panel.setDimensions(this.panel.width, this.panel.height);
+		BG.Panel.setTitle(this.panel.title);
+		BG.Panel.setContent(this.$panelHtml);
 		BG.Panel.centerPanel();
 		BG.Panel.$element.show();
 	}
 
 	dismissPanel() {
-		this.settings.template.choice = this.$templateInputs.filter( ':checked' ).val();
+		this.settings.template.choice = this.$templateInputs.filter(':checked').val();
 
 		// If the user enters the first time setup on a page, update the meta box.
-		if ( 'default' !== this.settings.template.choice && ! BoldgridEditor.is_boldgrid_theme ) {
-			let val = 'template/page/' + this.settings.template.choice + '.php';
-			$( '#page_template' )
-				.val( val )
-				.change();
+		if ('default' !== this.settings.template.choice && !BoldgridEditor.is_boldgrid_theme) {
+			const val = 'template/page/' + this.settings.template.choice + '.php';
+			$('#page_template').val(val).change();
 		}
 
 		// Make ajax call to save the given settings.
@@ -122,25 +120,24 @@ export class Notice extends Base {
 	}
 
 	saveSettings() {
-		let $inputs = BG.Panel.$element.find( 'input, select' ),
+		const $inputs = BG.Panel.$element.find('input, select'),
 			savedValues = $inputs.serializeArray();
 
-		$.ajax( {
+		$.ajax({
 			type: 'post',
 			url: ajaxurl,
 			dataType: 'json',
 			timeout: 20000,
-			data: $inputs.serialize()
-		} ).done( response => {
-
+			data: $inputs.serialize(),
+		}).done((response) => {
 			// If the user changes their default editor in the setup screen, reload page to add settings.
-			let inputName = `bgppb_post_type[${BoldgridEditor.post_type}]`,
-				input = savedValues.find( val => val.name === inputName );
+			const inputName = `bgppb_post_type[${BoldgridEditor.post_type}]`,
+				input = savedValues.find((val) => val.name === inputName);
 
-			if ( input && 'bgppb' !== input.value ) {
+			if (input && 'bgppb' !== input.value) {
 				window.location.reload();
 			}
-		} );
+		});
 	}
 
 	/**
@@ -149,20 +146,20 @@ export class Notice extends Base {
 	 * @since 1.6
 	 */
 	_setupStepActions() {
-		this.$panelHtml.on( 'boldgrid-editor-choose-color-palette', () => {
+		this.$panelHtml.on('boldgrid-editor-choose-color-palette', () => {
 			let $control;
 
 			$control = this.selection.create();
-			this.$panelHtml.find( '.choose-palette' ).html( $control );
+			this.$panelHtml.find('.choose-palette').html($control);
 
-			$control.one( 'palette-selection', () => {
-				this.$currentStep.find( '[data-action-step]' ).removeAttr( 'disabled' );
-			} );
+			$control.one('palette-selection', () => {
+				this.$currentStep.find('[data-action-step]').removeAttr('disabled');
+			});
 
-			$control.on( 'palette-selection', () => {
+			$control.on('palette-selection', () => {
 				this.settings.palette.choice = this.selection.getSelectedPalette();
-			} );
-		} );
+			});
+		});
 	}
 
 	/**
@@ -172,15 +169,15 @@ export class Notice extends Base {
 	 *
 	 * @param {string} step Step from the panel.
 	 */
-	_addPanelSettings( step ) {
-		this.$currentStep = this.$panelHtml.find( '[data-step="' + step + '"]' );
+	_addPanelSettings(step) {
+		this.$currentStep = this.$panelHtml.find('[data-step="' + step + '"]');
 
 		// Update Panel Settings.
-		BG.Panel.setTitle( this.$currentStep.data( 'panel-title' ) );
-		BG.Panel.setInfo( this.$currentStep.data( 'panel-info' ) );
+		BG.Panel.setTitle(this.$currentStep.data('panel-title'));
+		BG.Panel.setInfo(this.$currentStep.data('panel-info'));
 		BG.Panel.setDimensions(
-			this.$currentStep.data( 'panel-width' ) || this.panel.width,
-			this.$currentStep.data( 'panel-height' ) || this.panel.height
+			this.$currentStep.data('panel-width') || this.panel.width,
+			this.$currentStep.data('panel-height') || this.panel.height
 		);
 	}
 
@@ -190,19 +187,19 @@ export class Notice extends Base {
 	 * @since 1.6
 	 */
 	_setupNav() {
-		this.$panelHtml.find( '[data-action-step]' ).on( 'click', e => {
-			let $this = $( e.target ),
-				step = $this.data( 'action-step' );
+		this.$panelHtml.find('[data-action-step]').on('click', (e) => {
+			const $this = $(e.target),
+				step = $this.data('action-step');
 
-			this._addPanelSettings( step );
-			this.$panelHtml.trigger( 'boldgrid-editor-' + step );
-			this.$panelHtml.find( '.step' ).removeClass( 'active' );
+			this._addPanelSettings(step);
+			this.$panelHtml.trigger('boldgrid-editor-' + step);
+			this.$panelHtml.find('.step').removeClass('active');
 
 			BG.Panel.centerPanel();
 
-			this.$currentStep.addClass( 'active' );
-		} );
+			this.$currentStep.addClass('active');
+		});
 	}
 }
 
-export { Intro as default };
+export { Notice as default };
